3f3cdb9787004830a0bfae1822dd8037294cf695
Dang Tuan Anh <tuananh1909@gmail.com>
2024-07-03 07:33:45 +0900

    Fix docker

--- demo/run.sh
+++ demo/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mvn package
-java -Djava.library.path=$PATH:native/linux/x86_64 -cp `find target -name *.jar -printf %p:` com.example.Main
+java -cp `find target -name *.jar -printf %p:` com.example.Main
